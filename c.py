e49603f6f5fd6e3dbe8410719c732dbbafdc2804
ShiniYanada <40791830+ShiniYanada@users.noreply.github.com>
2020-06-15 01:49:28 +0900

    モジュールを使わないでTLEさせない方法

--- c.py
+++ c.py
@@ -1,10 +1,22 @@
-from math import gcd
- 
+def gcd(a, b):
+  if b == 0:
+    return a
+  else:
+    return gcd(b, a % b)
 ans = 0
 k = int(input())
 for i in range(1, k + 1):
-  for j in range(1, k + 1):
-    a = gcd(i, j)
-    for l in range(1, k + 1):
-      ans += gcd(a, l)
+  for j in range(i, k + 1):
+    for l in range(j, k + 1):
+      if i == j == l:
+        ans += i
+      elif i == j or j == l or i == l:
+        a = gcd(i, j)
+        a = gcd(a, l) 
+        ans = ans + a * 3
+      else:
+        a = gcd(i, j)
+        a = gcd(a, l)
+        ans = ans + a * 6
 print(ans)
+
